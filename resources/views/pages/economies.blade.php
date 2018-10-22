f93cabdd92e8e418ea5c960d87ab9fd27ba65717
DScode51 <fordavidsmithcode@gmail.com>
2018-10-22 19:00:16 +0300

    update content pages economie and politique  New file aside.php in template folder

--- resources/views/pages/economies.blade.php
+++ resources/views/pages/economies.blade.php
@@ -2,97 +2,226 @@
 
 @section('content')
     @include('templates.nav')
+<div class="politic_content">
 <div class="container-fluid">
-    <div class="col-sm-7 col-sm-offset-1">
-        <h1 class=""><strong>Économie</strong></h1>
-
-        <hr>
-        <div></div>
-        <hr>
-
-        <div class="row">
-            <div class="col-sm-12">
-                @foreach($article as $detail)
-                <a href="{{route('rubric.show', [strtolower($detail->rubric), $formatData($detail->start_at),$detail->id, $detail->slug])}}">
-                    <h4>{{$detail->title}}</h4>
-                    <img src="http://127.0.0.1/royaleAdmin/public/storage/pictures/{{$detail->pictures}}" style="width:100%" alt="Image">
-                    <p>{{$detail->content}}</p>
-                </a>
-                @endforeach
-            </div>
-        </div>
-        <hr>
-
+  <div class="row">
+    <div class="">
+      <h1 class="m-4"><strong>Econimie</strong></h1>
     </div>
-
-    <div class="col-sm-4 hidden-xs">
-        <hr>
-        <div class="col-sm-12">
-            <a href="#" class="btn btn-default"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-            <a href="#" class="btn btn-default"><i class="fa fa-google" aria-hidden="true"></i></a>
-            <a href="#" class="btn btn-default"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-            <button class="btn"><i class="fa fa-share" aria-hidden="true"></i> Partager</button>
-            <hr>
-        </div>
-
-        <div class="col-sm-12" style="background-color: #666; padding: 10px; border-radius: 10px; margin-bottom: 20px;">
-            <form method="post" action="" id="newslettres">
-                <div class="form-group form-group-sm col-sm-12">
-                    <label for="newslettres" class="">Newsletter Économie</label>
-                    <input type="email" name="" class="form-control" placeholder="Insèrez votre email" id="newslettres">
-                </div><br>
-                <button class="btn btn-defaultcol-md-4 col-xs-10" style="margin-left: 15px;" type="submit" name="" >Envoyer</button>
-            </form>
-        </div>
-
-
-        <div class="col-sm-12">
-            <h3 style="border-top: 4px solid #0080ff; margin-top: -5px; padding-top: 10px;"><strong><span style="color: red;">Info</span> plus</strong></h3>
-            <div class="list-group">
-
-                <a href="#" class="list-group-item ">
-                    <span class="badge pull-left">1</span>
-                    <h4 class="list-group-item-heading">List group item heading</h4>
-                    <p class="list-group-item-text">...lc[i ifjjf  qimimc  icim mmmmmmm mmmmmmmmmm cqimcqimc qmiqmciqcq mqcmiqmmc qimqciqiipq q </p>
-                </a>
-
-                <a href="#" class="list-group-item">
-                    <span class="badge pull-left">2</span>
-                    <h4 class="list-group-item-heading">List group item heading</h4>
-                    <p class="list-group-item-text">...</p>
-                </a>
-
-                <a href="#" class="list-group-item">
-                    <span class="badge pull-left">3</span>
-                    <h4 class="list-group-item-heading">List group item heading</h4>
-                    <p class="list-group-item-text">...</p>
-                </a>
-
-                <a href="#" class="list-group-item">
-                    <span class="badge pull-left">4</span>
-                    <h4 class="list-group-item-heading">List group item heading</h4>
-                    <p class="list-group-item-text">...</p>
-                </a>
-
-                <a href="#" class="list-group-item">
-                    <span class="badge pull-left">5</span>
-                    <h4 class="list-group-item-heading">List group item heading</h4>
-                    <p class="list-group-item-text">..fffffffffffffff ffffffffffffffffff fffffffffffffff ffffffffffffff .</p>
-                </a>
-            </div>
-            <hr>
-            <h4><a href="#">Titre</a></h4>
-            <p>...</p>
-            <hr>
-            <h4><a href="#">Titre</a></h4>
-            <p>...</p>
-            <hr>
-            <h4><a href="#">Titre</a></h4>
-            <p>...</p>
-            <hr>
-
-        </div>
-
-    </div>
+  </div>
+  <hr>
 </div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-8">
+      <hr>
+      <div class="row">
+        <div class="col-sm-12">
+            @foreach($article as $detail)
+            <a href="{{route('rubric.show', [strtolower($detail->rubric), $formatData($detail->start_at),$detail->id, $detail->slug])}}">
+                <h4>{{$detail->title}}</h4>
+                <img src="http://127.0.0.1/royaleAdmin/public/storage/pictures/{{$detail->pictures}}" style="width:100%" alt="Image">
+                <p>{{$detail->content}}</p>
+            </a>
+            @endforeach
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="row">
+            <!--Tous les article politiques cliques vont se retrouver ici-->
+          </div>
+        </div>
+        <div class="card-deck">
+          <div class="card mb-3 bg-white">
+            <a href="#">
+              <img class="img-fluid w-100 h-100" src="bookstore/images/880x495_551979.jpg" alt="First slide">
+            </a>
+            <h4 class="card-body"><a href="">Outside counsel in Kavanaugh hearing told Republican senators she would not have been able to prosecute</a></h4>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+          <div class="card mb-3">
+            <a href="#">
+              <img class="img-fluid w-100 h-100" src="bookstore/images/880x495_551979.jpg" alt="First slide">
+            </a>
+            <h4 class="card-body"><a href="">Outside counsel in Kavanaugh hearing told Republican senators she would not have been able to prosecute she would not have been able to prosecute</a></h4>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="card-deck">
+          <div class="card mb-3 bg-white">
+            <a href="#">
+              <img class="img-fluid w-100" src="bookstore/images/gvbuyb.jpg" alt="First slide">
+            </a>
+            <h4 class="card-body"><a href="">Outside counsel in Kavanaugh hearing </a></h4>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+          <div class="card mb-3">
+            <a href="#">
+              <img class="img-fluid w-100" src="bookstore/images/gvbuyb.jpg" alt="First slide">
+            </a>
+            <h4 class="card-body"><a href="">Outside counsel in Kavanaugh hearing told Republican senatsecute</a></h4>
+          </div>
+          <div class="card mb-3">
+            <a href="#">
+              <img class="img-fluid w-100" src="bookstore/images/gvbuyb.jpg" alt="First slide">
+            </a>
+            <h4 class="card-body"><a href="">Outside counsel in Kavanaugh hearing told Republican </a></h4>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="card-columns">
+          <div class="card bg-white">
+            <div class="card-body">
+              <h5 class="card-title"><a href="">Card title that wraps to a new line</a></h5>
+              <small class="text-muted">Politique</small>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title"><a href="">Card title that wraps to a new line</a></h5>
+              <small class="text-muted">Politique</small>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title"><a href="">Card title that wraps to a new line</a></h5>
+              <small class="text-muted">Politique</small>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title"><a href="">Card title that wraps to a new line</a></h5>
+              <small class="text-muted">Politique</small>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title"><a href="">Card title that wraps to a new line</a></h5>
+              <small class="text-muted">Politique</small>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title"><a href="">Card title that wraps to a new line</a></h5>
+              <small class="text-muted">Politique</small>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="row">
+        <ul class="list-unstyled">
+          <li class="media my-4 bg-white p-3">
+            <a href=""><img class="mr-3 img-fluid" src="https://placehold.it/150x100?text=Another Image Maybe" alt="Generic plac eholder image"></a>
+            <div class="media-body">
+              <a href=""><h5 class="mt-0 mb-1">List-based media object</h5></a>
+               <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+              <span class="text-muted">date and time</span>
+            </div>
+          </li>
+          <li class="media my-4 bg-white p-3">
+            <a href=""><img class="mr-3 img-fluid" src="https://placehold.it/150x100?text=Another Image Maybe" alt="Generic plac eholder image"></a>
+            <div class="media-body">
+              <a href=""><h5 class="mt-0 mb-1">List-based media object</h5></a>
+               <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+              <span class="text-muted">date and time</span>
+            </div>
+          </li>
+          <li class="media my-4 bg-white p-3">
+            <a href=""><img class="mr-3 img-fluid" src="https://placehold.it/150x100?text=Another Image Maybe" alt="Generic plac eholder image"></a>
+            <div class="media-body">
+              <a href=""><h5 class="mt-0 mb-1">List-based media object</h5></a>
+               <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+              <span class="text-muted">date and time</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <hr>
+
+      <div class="row pb-4 mb-4" style="background-color: #333;">
+        <div class="col-12 sections-titles">
+          <div class="row">
+            <h3 style="border-top: 4px solid #1abc9c; color: #fff; padding: 10px;" class="w-100 d-block text-center">
+              <strong>ROYALE NEWS TV</strong>
+              <hr class="mb-0">
+            </h3>
+          </div>
+
+          <div class="card-columns">
+            <div class="card">
+              <a href="#"><img class="img-fluid w-100" style="max-height: 400px;" src="bookstore/images/gvbuyb.jpg" alt="Card image cap">
+              <span class="badge badge-pill badge-primary">Chronique</span>
+              <div class="card-body border-bottom">
+                <h5>his is a longer card with supporting text below as a natural</h5>
+              </div></a>
+            </div>
+
+            <div class="card">
+              <a href="#"><img class="img-fluid w-100" style="max-height: 400px;" src="bookstore/images/gvbuyb.jpg" alt="Card image cap">
+              <span class="badge badge-pill badge-primary">Chronique</span>
+              <div class="card-body border-bottom">
+                <h5>his is a longer card with supporting text below as a natural</h5>
+              </div></a>
+            </div>
+
+            <div class="card">
+              <a href="#"><img class="img-fluid w-100" style="max-height: 400px;" src="bookstore/images/gvbuyb.jpg" alt="Card image cap">
+              <span class="badge badge-pill badge-primary">Chronique</span>
+              <div class="card-body border-bottom">
+                <h5>his is a longer card with supporting text below as a natural</h5>
+              </div></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <ul class="list-unstyled">
+          <li class="media my-4 bg-white p-3">
+            <a href=""><img class="mr-3 img-fluid" src="https://placehold.it/150x100?text=Another Image Maybe" alt="Generic plac eholder image"></a>
+            <div class="media-body">
+              <a href=""><h5 class="mt-0 mb-1">List-based media object</h5></a>
+               <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+              <span class="text-muted">date and time</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+      
+    <!--<?php
+       /*aside.ph ici*/
+    ?>-->
+
+  </div>
 @stop
---
+++ resources/views/templates/aside.blade.php
@@ -0,0 +1,71 @@
+@extends('templates.template')
+
+
+<div class="col-lg-4">
+      <div class="card w-100 mb-4">
+        <h5 class="p-2 card-header">Suivez-nous sur</h5>
+        <div class="card-body">
+          <a href="#" class="btn bg-dark"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          <a href="#" class="btn bg-dark"><i class="fa fa-google" aria-hidden="true"></i></a>
+          <a href="#" class="btn bg-dark"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        </div>
+      </div>
+
+      <div class="card w-100 mb-4">
+        <aside id="edition_day" class="">
+          <h5 class="text-center p-2 card-header mb-4" style="background-color: #333; color: #1abc9c;">L'edition du jour</h5>
+          <a href="#" title="Lire"><img class="img-fluid mx-auto d-block" src="bookstore/images/laune.jpg" style="width: 250px; height: 300px;">
+          </a>
+        </aside>
+      </div>
+
+      <div class="card w-100 mb-4">
+        <h5 class="p-2 card-header">Newsletter Politique</h5>
+        <form method="post" action="" id="newslettres" class="form-inline card-body">
+          <div class="form-group">
+            <input type="email" name="" class="form-control" placeholder="Insèrez votre email" id="newslettres">
+          </div>
+          <button class="btn bg-success" style="" type="submit" name="" >Envoyer</button>
+        </form>
+      </div>
+
+
+      <div class="card">
+        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+          <li class="nav-item col font-weight-bold">
+            <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">TOP INFOS</a>
+          </li>
+          <li class="nav-item col font-weight-bold">
+            <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">LES PLUS LUS</a>
+          </li>
+        </ul>
+        <div class="tab-content" id="pills-tabContent">
+          <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+            <ul class="list-group" style="overflow: auto; max-height: 400px;">
+              <li  class="list-group-item ">
+                <div class="w-100">
+                  <span class="badge-small p-2 mb-1 rounded font-weight-bold text-white">1</span>
+                  <small >3 days ago</small>
+                  <p class="mb-1"><a href="">List group item headingList group item headingList group item heading</a></p>
+                </div>
+              </li>
+
+              <li class="list-group-item text-right"><a href=""><small><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong> Voir tous</strong></small></a>
+              </li>
+            </ul>
+          </div>
+          <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+            <ul class="list-group list-group-flush" style="overflow: auto; max-height: 400px;">
+              <li class="list-group-item">
+                <div class="w-100">
+                  <small class="text-muted">Politique</small>
+                  <a href=""> Cras justo odio Last updated 3 mins agoLast updated 3 mins agoLast updated 3 mins ago</a>
+                </div>
+              </li>
+              
+              <li class="list-group-item text-right"><a href=""><small><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong> Voir tous</strong></small></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
